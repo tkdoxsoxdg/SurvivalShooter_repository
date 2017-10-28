980e582a113f866171ecc11997fe35df6d2df8f4
tkd\YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-10-28 18:02:08 +0900

    2017/10/27 18:00 commit

---
+++ SurvivalShooter/Assets/Scripts/Camera/CameraFllow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFllow : MonoBehaviour{
+
+    public Transform target;
+    public float smoothing = 5f; //係数（カメラの位置に関連→始点終点間） 
+
+    Vector3 offset;
+
+    void Start()
+    {
+        //オフセットの設定
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        //カメラポジションを設定。
+        Vector3 targetCamPos = target.position + offset;
+        /* カメラ追従の設定。
+         * 始点（プレイヤーの位置）から終点（カメラポジション）まで
+         * 1フレーム毎の時間（Time.deltaTime）経過による、カメラの動き。
+         * 係数を上げると終点に近づく。
+         */
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+    }
+
+}
