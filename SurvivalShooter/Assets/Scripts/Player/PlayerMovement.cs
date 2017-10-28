12716253c30b88a6a242b2c59e6bd86a54c18f15
tkd\YukiTakada <y.tkdoxsoxdg@gmail.com>
2017-10-28 14:19:13 +0900

    2017/10/27 14:19 commit

--- SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
+++ SurvivalShooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,18 +4,18 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6f;  //プレイヤー動作のスピード
 
-    Vector3 movement;  //プレイヤー動作を管理するVector
-    Animator anim; //animatorコンポーネント
-    Rigidbody playerRigitbody; //player Rigitbodyコンポーネント
-    int floormask; //レイヤー上のgameobjectの呼び出し
-    float camRayLength = 100f; //シーン上のカメラからのLength
+    Vector3 movement;  //3D動作の為の変数宣言（位置や方向）
+    Animator anim; //Animatorコンポーネントの変数宣言
+    Rigidbody playerRigitbody; //Rigitbodyコンポーネントの変数宣言
+    int floormask; //floormaskの変数宣言（レイヤー設定した）
+    float camRayLength = 100f; //レイのLength
 
     void Awake()
     {
         //フロアレイヤーにレイヤーマスクを作成
         floormask = LayerMask.GetMask("Floor");
 
-        //レイヤーマスクの設定
+        //レイヤーマスクの設定。AnimatorとRigidbodyのコンポーネントの呼び出し
         anim = GetComponent<Animator>();
         playerRigitbody = GetComponent<Rigidbody>();
 
@@ -23,63 +23,63 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        //input axesを入力
+        //input axesを入力。Horizontal→水平。Vertical→垂直
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
 
-        //シーン上でのプレイヤーの動き
+        //h,vを引数にMoveメソッドで動かす。
         Move(h, v);
 
-        //マウスカーソルに向かってプレイヤーが振り向く
+        //マウスカーソルに向かってプレイヤーが振り向く動作をTurning()で動かす。
         Turning();
 
-        //プレイヤーのアニメーション
+        //h,vを引数にAnimatingメソッドで動作させる
         Animating(h, v);
     }
 
-   void Move(float h, float v)
+   void Move(float h, float v) //プレイヤーの移動動作を設定
     {
         //axis inputの基本情報をセット
         movement.Set(h, 0f, v);
     
-        // 時間から動くスピードとVectorを比例し、movementを正規化
+        // 時間から動くスピードとVectorを比例し、movementを正規化し代入。
         movement = movement.normalized * speed * Time.deltaTime;
 
-        //プレイヤーの位置から追加するプレイヤーの動き
+        //playerRigitbody変数で呼び出したRigidbodyから、movement変数を代入したpositionを引数に、動作ポジションを設定。
         playerRigitbody.MovePosition(transform.position + movement);
     }
     
-     void Turning()
+     void Turning() //プレイヤーがマウスポインタ―に対して振り向く動作の設定
     {
-        //スクリーン上のマウスカーソルから、カメラのrayを作成
+        //mousePositionを引数に、カメラのスクリーンポイントからプレイヤの向きを変える。（マウスが画面上にあたっている方向に、プレイヤーが振り向くようにする）
         Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        //floorHit変数を定義
+        //レイを飛ばした際の、衝突したオブジェクト情報を得るための変数を定義。
         RaycastHit floorHit;
 
         //raycastの挙動とフロアレイヤーにヒットした時の処理
         if (Physics.Raycast(camRay, out floorHit, camRayLength, floormask))
         {
-            // Create a vector from the player to the point on the floor the raycast from the mouse hit.
+            // ヒットしたポイントからプレイヤー位置を減算した値を、playerToMouseに代入しインスタンスを作成。playerToMouse→プレイヤーの視点
             Vector3 playerToMouse = floorHit.point - transform.position;
 
-            //Ensure the vector is entirely along the floor plane.
+            //y軸に対しては、無視する（設定しないように、0fを設定）
             playerToMouse.y = 0f;
 
-            // Create a quaternion (rotation) based on looking down the vector from the player to the mouse.
+            //クォータニオンにて回転
             Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
 
-            // Set the player's rotation to this new rotation
+            // 回転する際の動作を定義。
             playerRigitbody.MoveRotation(newRotation);
         }
     }
 
-    void Animating(float h, float v)
+    void Animating(float h, float v) // プレイヤーが移動するときのアニメーションの設定
     {
-        // Create a boolean that is true if either of the input axes is non-zero.
+        // walkingの判定
         bool walking = h != 0f || v != 0f;
 
-        // Tell the animator whether or not the player is walking.
+        // anim変数に対して判定を設定。その時の引数も同時に定義する。
         anim.SetBool("IsWalking", walking);
     }
 
